0b3caa8a788f2dae6ec6b2c21501039a1c86e3db
Hubert <lubulahubert@gmail.com>
2026-03-11 18:40:29 +0300

    add restaurant-front-end

---
+++ restaurant-frontend/src/data.js
@@ -0,0 +1,24 @@
+
+export const menuItems = [
+  {
+    id: 1,
+    name: 'Pizza Margherita',
+    description: 'Tomates, mozzarella, basilic',
+    price: 500,
+    image: './boulayi.jpg'
+  },
+  {
+    id: 2,
+    name: 'Burger classique',
+    description: 'Boeuf, salade, tomate, sauce',
+    price: 300,
+    image: 'https://via.placeholder.com/100'
+  },
+  {
+    id: 3,
+    name: 'Pâtes Carbonara',
+    description: 'Pâtes, crème, lardons, parmesan',
+    price: 400,
+    image: 'https://via.placeholder.com/100'
+  }
+];
